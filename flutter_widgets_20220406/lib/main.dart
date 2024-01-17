109d68012602080e75402ca581efa40e213240cf
Verti <a20220406@utem.edu.mx>
2024-01-17 16:54:58 -0600

    Cambios en los comentarios

--- flutter_widgets_20220406/lib/main.dart
+++ flutter_widgets_20220406/lib/main.dart
@@ -4,37 +4,44 @@ void main() {
   runApp(const MyApp());
 }
 
+// Define the main app widget
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // Create a MaterialApp widget
     return MaterialApp(
       title: 'App De Verti',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
+      // Set the home page to MyHomePage widget
       home: const MyHomePage(),
     );
   }
 }
 
+// Define the home page widget
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // Create a Scaffold widget
     return Scaffold(
       appBar: AppBar(
         title: const Text('App De Verti'),
         backgroundColor: Colors.blue,
       ),
+      // Set the body to TextInputWidget widget
       body: const TextInputWidget(),
     );
   }
 }
 
+// Define a stateful widget for text input
 class TextInputWidget extends StatefulWidget {
   const TextInputWidget({Key? key}) : super(key: key);
 
@@ -42,13 +49,16 @@ class TextInputWidget extends StatefulWidget {
   State<TextInputWidget> createState() => _TextInputWidgetState();
 }
 
+// Define the state for the text input widget
 class _TextInputWidgetState extends State<TextInputWidget> {
   final controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    // Create a column widget to display the text input and button
     return Column(
-      children: <Widget> [
+      children: <Widget>[
+        // Create a text input field
         TextField(
           controller: controller,
           decoration: const InputDecoration(
@@ -56,6 +66,7 @@ class _TextInputWidgetState extends State<TextInputWidget> {
             labelText: 'Type a message',
           ),
         ),
+        // Create a button to show the entered message in an alert dialog
         TextButton(
           onPressed: () {
             showDialog(
